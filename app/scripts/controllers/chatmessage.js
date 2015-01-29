efdc6d7151a6fb2b49dc2c36771e2ce14e787a3c
pocketmax <sadsdf@adfsadf.com>
2015-01-29 04:48:47 -0700

    * added static tags

--- app/scripts/controllers/chatmessage.js
+++ app/scripts/controllers/chatmessage.js
@@ -8,13 +8,41 @@
  * Controller of the tagchatApp
  */
 angular.module('tagchatApp')
-  .controller('ChatmessageCtrl', function ($scope, dataLink) {
+  .controller('ChatmessageCtrl', function ($scope, $q, $filter, timestamp) {
+
+    var tags = [
+      'angularJS',
+      'bootstrapJS',
+      'java',
+      'mongoDB',
+      'nodeJS',
+      'phonegap',
+      'touch',
+      'extjs',
+      'css',
+      'sass',
+      'compass',
+      'less'
+    ];
+
+    $scope.loadTags = function(query) {
+      var q = $q.defer();
+      q.resolve($filter('filter')(tags, query));
+      return q.promise;
+    };
 
     $scope.addMessage = function(){
-      if(!$scope.newMessage){
-        return false;
+
+      var tmpTags = [];
+      for(var i in $scope.tags){
+        tmpTags.push($scope.tags[i].text);
       }
-      dataLink.messages.push({ message: $scope.newMessage});
+
+      $scope.items[timestamp]={
+        from: 'bla',
+        msg: $scope.newMessage,
+        tags: tmpTags
+      };
       $scope.newMessage='';
     };
 
