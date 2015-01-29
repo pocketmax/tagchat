314c4351f33226f6174ba4696cf15ab29594afa5
pocketmax <sadsdf@adfsadf.com>
2015-01-29 04:03:20 -0700

    * added angular dependency files for unit tests to work

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,6 +26,11 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-scroll-glue/src/scrollglue.js',
+      'bower_components/firebase/firebase.js',
+      'bower_components/angularfire/dist/angularfire.js',
+      'bower_components/ng-tags-input/ng-tags-input.js',
+      'bower_components/angular-md5/angular-md5.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
@@ -63,7 +68,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
     // proxies: {
