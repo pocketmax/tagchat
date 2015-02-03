0dc159797fd424571611c80732b429ff281b4316
pocketmax <sadsdf@adfsadf.com>
2015-02-02 21:39:44 -0700

    * added logout support

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -9,24 +9,19 @@
  */
 angular.module('tagchatApp')
     .controller('LoginCtrl', ['store','$scope','auth',function (store, $scope, auth) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
-
         $scope.login = function () {
             auth.signin({}, function (profile, token) {
                 // Success callback
                 store.set('profile', profile);
                 store.set('token', token);
-                alert('WOO HOO!!');
-            }, function (arg1,arg2) {
+            }, function (arg1) {
                 console.log(arg1);
-                console.log(arg2);
-                // Error callback
-                alert('DOH!!');
             });
+        };
+        $scope.logout = function () {
+            auth.signout();
+            store.remove('profile');
+            store.remove('token');
         }
 
     }]);
