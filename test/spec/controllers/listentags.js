1a4ed82aabcdcb9d8f76ef8172d3795e96edc0d1
pocketmax <sadsdf@adfsadf.com>
2015-02-05 01:20:40 -0700

    unit test for listentags controller ready

--- test/spec/controllers/listentags.js
+++ test/spec/controllers/listentags.js
@@ -2,21 +2,44 @@
 
 describe('Controller: ListentagsCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('tagchatApp'));
+    // load the controller's module
+    beforeEach(module('tagchatApp'));
 
-  var ListentagsCtrl,
-    scope;
+    var ListentagsCtrl,
+        listenTags = [{text: 'a'}],
+        store,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    ListentagsCtrl = $controller('ListentagsCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        store = new function () {
+            var core = {};
+
+            this.get = function (field) {
+                return core[field];
+            };
+
+            this.set = function (field, data) {
+                core[field] = data;
+            };
+
+        };
+
+        ListentagsCtrl = $controller('ListentagsCtrl', {
+            $scope: scope,
+            store: store,
+            listenTags: listenTags
+        });
+    }));
+
+    it('should set $scope.listenTags', function () {
+        expect(scope.listenTags).toBe(listenTags);
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+    it('should cache listenTag to local hd', function () {
+        scope.cacheTags();
+        expect(store).toBeDefined();
+        expect(store.get('listenTags')).toBe(listenTags);
+    });
 });
