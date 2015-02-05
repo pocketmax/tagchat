b244e28389f004fffed797f87ff122dabcf1caa7
pocketmax <sadsdf@adfsadf.com>
2015-02-05 14:03:52 -0700

    issue #18

--- test/spec/services/db.js
+++ test/spec/services/db.js
@@ -6,11 +6,14 @@ describe('Service: db', function () {
   beforeEach(module('tagchatApp'));
 
   // instantiate service
-  var db;
+  var firebase,
+      routeParams = { roomtId: 'test' };
+
   beforeEach(inject(function (_db_) {
     db = _db_;
   }));
 
+  // TODO: figure out how to properly test a service that injects third party objects
   it('should do something', function () {
     expect(!!db).toBe(true);
   });
