c870840d94ffc0f0bc83efdcda1b91a190908447
pocketmax <sadsdf@adfsadf.com>
2015-02-05 12:37:40 -0700

    issue #14

--- test/spec/controllers/chatmessage.js
+++ test/spec/controllers/chatmessage.js
@@ -12,41 +12,54 @@ describe('Controller: ChatmessageCtrl', function () {
       Db;
 
   beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+    scope = { msgs: {}, newMessage: 'my message' };
     auth = { profile: { nickname: 'test' } };
-    Db.timestamp = 'stamp';
+    Db = { timestamp: 'stamp'};
     ChatmessageCtrl = $controller('ChatmessageCtrl', {
       $scope: scope,
       auth: auth,
       Db: Db
     });
-
-
+    scope.addMessage();
   }));
 
   describe('message object', function () {
-
     it('should populate from field', function () {
-      expect(1).toBe(1);
+      var msg;
+      for(var i in scope.msgs){
+        msg = scope.msgs[i];
+      }
+      expect(msg.from).toBe('test');
     });
 
     it('should populate the inserted date and time field', function () {
-      expect(1).toBe(1);
+      var msg;
+      for(var i in scope.msgs){
+        msg = scope.msgs[i];
+      }
+      expect(msg.insertedDtTm).toBe('stamp');
     });
 
     it('should populate the message field', function () {
-      expect(1).toBe(1);
+      var msg;
+      for(var i in scope.msgs){
+        msg = scope.msgs[i];
+      }
+      expect(msg.msg).toBe('my message');
     });
 
     it('should populate the tags field', function () {
-//    expect(scope.awesomeThings.length).toBe(3);
-      expect(1).toBe(1);
+      var msg;
+      for(var i in scope.msgs){
+        msg = scope.msgs[i];
+      }
+      expect(msg.tags).toBe(tags);
     });
 
   });
 
   it('should clear the new message field', function () {
-    expect(1).toBe(1);
+    expect(scope.newMessage).toBe('');
   });
 
 
