f4f701fb6561c3b6101580fc6d7c0a8912051f2c
pocketmax <sadsdf@adfsadf.com>
2015-02-04 21:58:07 -0700

    issue #13 took tags filter out

--- app/scripts/controllers/chatwindow.js
+++ app/scripts/controllers/chatwindow.js
@@ -8,31 +8,6 @@
  * Controller of the tagchatApp
  */
 angular.module('tagchatApp')
-  .filter('tags', function(){
-
-    return function(items, listenTags){
-
-      if(!listenTags || !listenTags.length){
-        return items;
-      }
-
-      var filtered = {};
-      for(var i in items){
-        var item = items[i];
-        if( item && typeof item === 'object' ){
-            for(var x in listenTags){
-              if( item.tags[listenTags[x].text] ){
-                filtered[i]=item;
-                continue;
-              }
-            }
-        }
-      }
-
-      return filtered;
-    };
-
-  })
   .controller('ChatwindowCtrl', ['$scope', function ($scope) {
 
 
