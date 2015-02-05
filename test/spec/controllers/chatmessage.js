b3e0d1d141a35c43ad7896e0486a77e8d31dc64e
pocketmax <sadsdf@adfsadf.com>
2015-02-05 12:00:10 -0700

    issue #14

--- test/spec/controllers/chatmessage.js
+++ test/spec/controllers/chatmessage.js
@@ -5,23 +5,49 @@ describe('Controller: ChatmessageCtrl', function () {
   // load the controller's module
   beforeEach(module('tagchatApp'));
 
-  var ChatmessageCtrl, scope;
-  var q = {};
-  var filter = {};
+  var
+      ChatmessageCtrl,
+      scope,
+      auth,
+      Db;
 
-  // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
+    auth = { profile: { nickname: 'test' } };
+    Db.timestamp = 'stamp';
     ChatmessageCtrl = $controller('ChatmessageCtrl', {
       $scope: scope,
-      $q: q,
-      $filter: filter
+      auth: auth,
+      Db: Db
     });
+
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  describe('message object', function () {
+
+    it('should populate from field', function () {
+      expect(1).toBe(1);
+    });
+
+    it('should populate the inserted date and time field', function () {
+      expect(1).toBe(1);
+    });
+
+    it('should populate the message field', function () {
+      expect(1).toBe(1);
+    });
+
+    it('should populate the tags field', function () {
 //    expect(scope.awesomeThings.length).toBe(3);
-    expect(1).toBe(1);
+      expect(1).toBe(1);
+    });
 
   });
+
+  it('should clear the new message field', function () {
+    expect(1).toBe(1);
+  });
+
+
 });
