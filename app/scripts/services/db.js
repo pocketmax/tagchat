8148455d2cadfe402263fee1add94ab899b0a951
pocketmax <sadsdf@adfsadf.com>
2015-02-04 20:40:06 -0700

    issue #12 wrapped firebase module in db service for easier unit testing

---
+++ app/scripts/services/db.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tagchatApp.db
+ * @description
+ * # db
+ * Service in the tagchatApp.
+ */
+angular.module('tagchatApp')
+  .service('Db', ['$firebase','$routeParams',function Db($firebase, $routeParams) {
+
+      var ref = new Firebase('https://burning-fire-3434.firebaseio.com/tagchat/chatrooms/' + $routeParams.roomId + "/messages");
+      var sync = $firebase(ref);
+      var syncObject = sync.$asObject();
+
+      this.bind = function(item, field){
+        syncObject.$bindTo(item, field);
+      };
+
+  }]);
