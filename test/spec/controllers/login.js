6bce17d8bf347014206ddacbd4dbb283f9a9bb86
pocketmax <sadsdf@adfsadf.com>
2015-02-05 20:56:53 -0700

    fixed #16 #15

--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -2,77 +2,96 @@
 
 describe('Controller: LoginCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('tagchatApp'));
+    // load the controller's module
+    beforeEach(module('tagchatApp'));
 
-  var LoginCtrl,
-      store = {},
-      scope = {},
-      auth = { signout: function(){} };
+    var LoginCtrl,
+        store = {},
+        scope = {},
+        auth;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller) {
-    store = new function () {
-      var core = {};
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller) {
 
-      this.get = function (field) {
-        return core[field];
-      };
+        store = new function () {
+            var core = {};
 
-      this.set = function (field, data) {
-        core[field] = data;
-      };
+            this.get = function (field) {
+                return core[field];
+            };
 
-      this.remove = function (field) {
-        delete core[field];
-      };
+            this.set = function (field, data) {
+                core[field] = data;
+            };
 
-    };
-    LoginCtrl = $controller('LoginCtrl', {
-      $scope: scope,
-      store: store,
-      auth: auth
-    });
-  }));
+            this.remove = function (field) {
+                delete core[field];
+            };
 
-  describe('login()', function(){
+        };
 
-    // TODO: figure out how to unit test async code
-    it('should do something', function () {
-      expect(0).toBe(0);
-    });
+        auth = {
+            signin: function (obj, succCb) {
+                console.log('MOCK SIGNIN!!!!');
+                succCb({}, {});
+            },
+            signout: function () {
 
-  });
+            }
+        };
 
-  describe('logout()', function(){
+        LoginCtrl = $controller('LoginCtrl', {
+            $scope: scope,
+            store: store,
+            auth: auth
+        });
 
-    it('should run auth.signout()', function () {
-      spyOn(auth, 'signout');
-      scope.logout();
-      expect(auth.signout).toHaveBeenCalled();
-    });
+    }));
 
-    it('should remove cached profile object', function () {
-      spyOn(store, 'remove');
-      scope.logout();
-      expect(store.remove).toHaveBeenCalledWith('profile');
-      expect(store.get('profile')).toBeUndefined();
-    });
+    describe('login()', function () {
 
-    it('should remove cached token object', function () {
-      spyOn(store, 'remove');
-      scope.logout();
-      expect(store.remove).toHaveBeenCalledWith('token');
-      expect(store.get('token')).toBeUndefined();
+        it('should call auth.signin()', function () {
+
+            spyOn(auth, 'signin').and.callThrough();
+            spyOn(store, 'set');
+
+            scope.login();
+            expect(auth.signin).toHaveBeenCalled();
+            expect(store.set.calls.count()).toBe(2);
+
+        });
+
     });
 
-    it('should remove cached localTags object', function () {
-      spyOn(store, 'remove');
-      scope.logout();
-      expect(store.remove).toHaveBeenCalledWith('localtag');
-      expect(store.get('localTags')).toBeUndefined();
-    });
+    describe('logout()', function () {
 
-  });
+        it('should run auth.signout()', function () {
+            spyOn(auth, 'signout');
+            scope.logout();
+            expect(auth.signout).toHaveBeenCalled();
+        });
+
+        it('should remove cached profile object', function () {
+            spyOn(store, 'remove');
+            scope.logout();
+            expect(store.remove).toHaveBeenCalledWith('profile');
+            expect(store.get('profile')).toBeUndefined();
+        });
+
+        it('should remove cached token object', function () {
+            spyOn(store, 'remove');
+            scope.logout();
+            expect(store.remove).toHaveBeenCalledWith('token');
+            expect(store.get('token')).toBeUndefined();
+        });
+
+        it('should remove cached localTags object', function () {
+            spyOn(store, 'remove');
+            scope.logout();
+            expect(store.remove).toHaveBeenCalledWith('localTags');
+            expect(store.get('localTags')).toBeUndefined();
+        });
+
+    });
 
 });
